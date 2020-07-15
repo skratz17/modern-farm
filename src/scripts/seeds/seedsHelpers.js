9cbc44c7c7b17efe4cb4db64aaaa00bc445469ee
Jacob Eckert <jweckert17@gmail.com>
2020-07-15 11:49:07 -0500

    refactor - removed local getSeed function from tractor.js and moved to exported getSeedFactoryFunction function in seedsHelpers.js

---
+++ src/scripts/seeds/seedsHelpers.js
@@ -0,0 +1,30 @@
+import { createAsparagus } from './asparagus.js';
+import { createCorn } from './corn.js';
+import { createPotato } from './potato.js';
+import { createSoybean } from './soybean.js';
+import { createSunflower } from './sunflower.js';
+import { createWheat } from './wheat.js';
+
+/**
+ * Given a plant type, return that plant's corresponding seed factory function (e.g., "Asparagus" -> createAsparagus).
+ * Throws Error if plant type is not recognized as a valid plant type.
+ * @param {String} plantType The name of the plant to obtain the seed factory function for.
+ */
+export const getSeedFactoryFunction = plantType => {
+  switch(plantType) {
+    case 'Asparagus':
+      return createAsparagus;
+    case 'Corn':
+      return createCorn;
+    case 'Potato':
+      return createPotato;
+    case 'Soybean':
+      return createSoybean;
+    case 'Sunflower':
+      return createSunflower;
+    case 'Wheat':
+      return createWheat;
+    default:
+      throw new Error(`${plantType} is not a valid plant type.`);
+  }
+}
--- src/scripts/tractor.js
+++ src/scripts/tractor.js
@@ -1,18 +1,12 @@
 import { addPlant } from './field.js';
-
-import { createAsparagus } from './seeds/asparagus.js';
-import { createCorn } from './seeds/corn.js';
-import { createPotato } from './seeds/potato.js';
-import { createSoybean } from './seeds/soybean.js';
-import { createSunflower } from './seeds/sunflower.js';
-import { createWheat } from './seeds/wheat.js';
+import { getSeedFactoryFunction } from './seeds/seedsHelpers.js';
 
 export const plantSeeds = plan => {
   plan.forEach(row => {
     row.forEach(plantType => {
       try {
-        const seed = getSeed(plantType);
-        addPlant(seed);
+        const seedFactoryFunction = getSeedFactoryFunction(plantType);
+        addPlant(seedFactoryFunction());
       }
       catch(e) {
         console.log(e.message);
@@ -20,27 +14,3 @@ export const plantSeeds = plan => {
     });
   });
 };
-
-/**
- * Given a plant type, return that plant's corresponding seed as generated from one of the factory functions defined in seeds/ directory
- * Throws Error if plant type is not recognized as a valid plant type.
- * @param {String} plantType The name of the plant to grow
- */
-const getSeed = plantType => {
-  switch(plantType) {
-    case 'Asparagus':
-      return createAsparagus();
-    case 'Corn':
-      return createCorn();
-    case 'Potato':
-      return createPotato();
-    case 'Soybean':
-      return createSoybean();
-    case 'Sunflower':
-      return createSunflower();
-    case 'Wheat':
-      return createWheat();
-    default:
-      throw new Error(`${plantType} is not a valid plant type.`);
-  }
-}

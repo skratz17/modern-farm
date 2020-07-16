3d7b3a5c89f3f8e41ee8da9e131c44d6e87358e9
Jacob Eckert <jweckert17@gmail.com>
2020-07-16 11:59:14 -0500

    implemented sorting of harvest in harvester.js

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -11,5 +11,19 @@ export const harvestPlants = plants => {
     }
   });
 
-  return seeds;
+  return sortPlants(seeds);
+};
+
+/**
+ * Given an array of plant objects, returns a new array of plants sorted alphabetically by type, case-insensitive.
+ * @param {Array} plants Array of plant objects to sort 
+ */
+const sortPlants = plants => {
+  const sortedPlants = plants.slice();
+
+  sortedPlants.sort((plantA, plantB) => 
+    plantA.type.toLowerCase().localeCompare(plantB.type.toLowerCase())
+  );
+
+  return sortedPlants;
 };

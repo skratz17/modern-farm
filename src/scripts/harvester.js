c28a50e82f77f4c071d285feb0ef520427111883
Jacob Eckert <jweckert17@gmail.com>
2020-07-16 13:34:34 -0500

    moved plant sorting logic from harvester.js to field.js

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -11,19 +11,5 @@ export const harvestPlants = plants => {
     }
   });
 
-  return sortPlants(seeds);
-};
-
-/**
- * Given an array of plant objects, returns a new array of plants sorted alphabetically by type, case-insensitive.
- * @param {Array} plants Array of plant objects to sort 
- */
-const sortPlants = plants => {
-  const sortedPlants = plants.slice();
-
-  sortedPlants.sort((plantA, plantB) => 
-    plantA.type.toLowerCase().localeCompare(plantB.type.toLowerCase())
-  );
-
-  return sortedPlants;
+  return seeds;
 };
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 import { createPlan } from './plan.js';
 import { plantSeeds } from './tractor.js';
-import { usePlants } from './field.js';
+import { usePlantsSorted } from './field.js';
 import { harvestPlants } from './harvester.js';
 import { Catalog } from './catalog/Catalog.js';
 
@@ -8,7 +8,7 @@ const yearlyPlan = createPlan();
 
 plantSeeds(yearlyPlan);
 
-const plants = usePlants();
+const plants = usePlantsSorted();
 const harvest = harvestPlants(plants);
 
 Catalog(harvest);

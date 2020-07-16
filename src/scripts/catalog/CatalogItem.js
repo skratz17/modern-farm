21cd81fd32ad25bdd88575bf0f4cb8c3753cb65f
Jacob Eckert <jweckert17@gmail.com>
2020-07-16 15:54:12 -0500

    added icons

--- src/scripts/catalog/CatalogItem.js
+++ src/scripts/catalog/CatalogItem.js
@@ -1,9 +1,11 @@
 import escape from '../utils/escapeHTML.js';
 
 export const CatalogItem = item => {
-  const { type, id } = item;
+  const { type, id, icon } = item;
 
   return `
-    <section id=${id} class="plant">${escape(type)}</section>
+    <section id=${id} class="plant">
+      <img src=${encodeURI(icon)}>
+    </section>
   `;
 };
